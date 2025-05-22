c9563ca0c2e6fbe02449cf2d2ccf80e24e00c8b9
Stephen <81497928+eigmax@users.noreply.github.com>
2025-05-22 18:50:41 +0800

    fix: ci key refill (#110)

--- scripts/bitcoin-init.sh
+++ scripts/bitcoin-init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+RPC_USER=111111
+RPC_PSWD=111111
+WALLET_NAME=alice
+WALLET_PASSPHRASE=btcstaker
 
 bitcoind -daemon \
    -server=1  \
@@ -9,39 +13,37 @@ bitcoind -daemon \
    -fallbackfee='0.01' \
    -rpcallowip=0.0.0.0/0 \
    -rpcbind=0.0.0.0 \
-   -rpcuser=111111 \
-   -rpcpassword=111111
+   -rpcuser=$RPC_USER \
+   -rpcpassword=$RPC_PSWD
 
 # Wait until RPC is ready
 sleep 5
-while ! bitcoin-cli -regtest -rpcuser=111111 -rpcpassword=111111 getblockchaininfo > /dev/null 2>&1; do
+while ! bitcoin-cli -regtest -rpcuser=$RPC_USER -rpcpassword=$RPC_PSWD getblockchaininfo > /dev/null 2>&1; do
   echo "Waiting for bitcoind..."
   sleep 2
 done
 
 # install bitcoin-cli on MacOS: `brew install bitcoin`
-export BTC="${USE_DOCKER} bitcoin-cli -regtest -rpcuser=111111 -rpcpassword=111111"
+export BTC="${USE_DOCKER} bitcoin-cli -regtest -rpcuser=$RPC_USER -rpcpassword=$RPC_PSWD"
 
 $BTC -named createwallet \
-    wallet_name=alice \
-    passphrase="btcstaker" \
+    wallet_name=$WALLET_NAME \
+    passphrase=$WALLET_PASSPHRASE \
     load_on_startup=true \
     descriptors=false
 
-$BTC loadwallet "alice"
+$BTC loadwallet $WALLET_NAME
 
-$BTC --rpcwallet=alice walletpassphrase "btcstaker" 600
-
-$BTC --rpcwallet=alice -generate 1000
+$BTC --rpcwallet=$WALLET_NAME walletpassphrase $WALLET_PASSPHRASE 600
 
 address="bcrt1q7tr8sl50zanztcrps35hakqpe7gmfzedhhnxcspj7n0ks5lyrnhs6m8ewg"
 ## fund the address
-$BTC --rpcwallet=alice sendtoaddress $address 1
-$BTC --rpcwallet=alice -generate 10
+$BTC --rpcwallet=$WALLET_NAME sendtoaddress $address 1
+$BTC --rpcwallet=$WALLET_NAME -generate 101
 
 # Install watch
 apt-get update
 apt-get install -y procps
 # A terminal-based program (like watch, top, less, etc.) runs in an environment, TERM environment variable should be set
 export TERM=xterm
-watch -n 2 "$BTC -generate 1"
+watch -n 2 "$BTC -generate 1 && $BTC keypoolrefill"
